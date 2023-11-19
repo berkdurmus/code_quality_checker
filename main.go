704d1510fe184bbd6469fd56c71f68bea993038e
Berk Durmus <berkdurmus@yahoo.com>
2023-11-19 09:40:23 +0300

    feat(quality): extend the logic
    Extend code quality check logic

--- main.go
+++ main.go
@@ -2,9 +2,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -14,20 +17,76 @@ func main() {
 		return
 	}
 	sourcePath := args[0]
+
+	// Metrics
+	totalLines := 0
+	totalCommentLines := 0
+	totalFunctionCount := 0
+	// Additional metrics can be declared here
+
 	err := filepath.Walk(sourcePath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Printf("Error accessing path %q: %v
-", path, err)
+			fmt.Printf("Error accessing path %q: %v\n", path, err)
 			return err
 		}
-		if !info.IsDir() {
-			fmt.Println("Analyzing file:", path)
-			// Add code analysis logic here
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
+			fileMetrics, err := analyzeFile(path)
+			if err != nil {
+				fmt.Printf("Error analyzing file %s: %v\n", path, err)
+				return err
+			}
+			totalLines += fileMetrics.totalLines
+			totalCommentLines += fileMetrics.commentLines
+			totalFunctionCount += fileMetrics.functionCount
+			// Add additional metric aggregations here
 		}
 		return nil
 	})
 	if err != nil {
-		fmt.Printf("Error walking the path %q: %v
-", sourcePath, err)
+		fmt.Printf("Error walking the path %q: %v\n", sourcePath, err)
+		return
 	}
+
+	// Print out the metrics
+	fmt.Printf("Total Lines: %d\n", totalLines)
+	fmt.Printf("Total Comment Lines: %d\n", totalCommentLines)
+	fmt.Printf("Total Function Count: %d\n", totalFunctionCount)
+	// Print additional metrics here
+}
+
+// FileMetrics holds the metrics for a single file
+type FileMetrics struct {
+	totalLines     int
+	commentLines   int
+	functionCount  int
+	// Additional metrics fields can be added here
+}
+
+// analyzeFile analyzes a single Go file and returns its metrics
+func analyzeFile(filePath string) (FileMetrics, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return FileMetrics{}, err
+	}
+	defer file.Close()
+
+	var metrics FileMetrics
+	scanner := bufio.NewScanner(file)
+	functionRegex := regexp.MustCompile(`^func\s`)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		metrics.totalLines++
+
+		trimmedLine := strings.TrimSpace(line)
+		if strings.HasPrefix(trimmedLine, "//") {
+			metrics.commentLines++
+		}
+		if functionRegex.MatchString(trimmedLine) {
+			metrics.functionCount++
+		}
+		// Add additional metric calculations here
+	}
+
+	return metrics, scanner.Err()
 }
